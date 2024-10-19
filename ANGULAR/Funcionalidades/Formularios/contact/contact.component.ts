1ceed6f3086c657ca07d8b039348ce29b31e0b42
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-10-19 11:29:19 +0200

    Add files via upload

---
+++ ANGULAR/Funcionalidades/Formularios/contact/contact.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+        public usuario: any = {
+          nombre: '',
+          email: ''
+        }
+
+        enviar(){
+          console.log(this.usuario)
+        }
+}

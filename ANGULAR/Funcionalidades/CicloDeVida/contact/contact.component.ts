d3b5ece94b4f021e8162a21fb321a9a9ea63298a
Gonzalo <129194237+gonzaloH8@users.noreply.github.com>
2024-10-20 13:26:12 +0200

    Add files via upload

---
+++ ANGULAR/Funcionalidades/CicloDeVida/contact/contact.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit{
+
+  formularioContacto: FormGroup
+  tipoDni: string = ''
+  mostrarDni: boolean = false
+
+  constructor(private form: FormBuilder){
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      tipoDni: [''],
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  ngOnInit(): void {
+    this.formularioContacto.get('nombre')?.setValue('Juan')
+    this.formularioContacto.get('nombre')?.disable()
+    this.formularioContacto.get('tipoDni')?.valueChanges.subscribe(value => {
+      this.mostrarDni = value != ''
+      this.tipoDni = value
+    })
+  }
+
+  ngOnDestroy(): void{
+    console.log('Se destruyo el componente')
+  }
+
+  hasErrors(controlName: string, errorType: string){
+    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched
+  }
+        
+  enviar(){
+    console.log(this.formularioContacto)
+  }
+}
